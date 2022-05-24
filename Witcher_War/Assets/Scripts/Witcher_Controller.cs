3076a6b128b9fe04a4e48a5ceec72404e9e2073a
williamsfacundo <facuwilli@gmail.com>
2022-05-24 16:27:18 -0300

    Added movement var to the witcher controller.

--- Witcher_War/Assets/Scripts/Witcher_Controller.cs
+++ Witcher_War/Assets/Scripts/Witcher_Controller.cs
@@ -2,5 +2,32 @@ using UnityEngine;
 
 public class Witcher_Controller : MonoBehaviour
 {
+    [SerializeField] WITCHER_TYPE witcherType = WITCHER_TYPE.CPU;
+
+    private ICanMove movement;
     
+    private void Start()
+    {
+        SetMovement(witcherType);                
+    }
+
+    private void Update()
+    {
+        movement.Move();
+    }
+
+    void SetMovement(WITCHER_TYPE witcherType) 
+    {
+        switch (witcherType) 
+        {
+            case WITCHER_TYPE.PLAYER:
+
+                movement = new Player_Movement();
+                break;
+            case WITCHER_TYPE.CPU:
+
+                movement = new CPU_Movement();
+                break;
+        }
+    }
 }
