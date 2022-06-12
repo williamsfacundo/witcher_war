07bcda428752d2ad8db5c9517c9cef4043f6f073
facundo <facuwilli@gmail.com>
2022-06-12 18:19:31 -0300

    Added wall script and atache it to wall prefab.

---
+++ Witcher_War/Assets/Scripts/Map/Wall.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private Tile tile;
+    
+    private Vector2 initialPosIndex;
+
+    public Vector2 InitialPosIndex
+    {
+        set
+        {
+            initialPosIndex = value;
+        }
+    }
+
+    private void Start()
+    {
+        Tile_Map.SetObjectTile(initialPosIndex, ref tile);
+
+        transform.position = Tile_Map.TileMap[(int)initialPosIndex.y, (int)initialPosIndex.x].Position;
+    }    
+}
