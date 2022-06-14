ea99a45dd71d503298e1f10ca7eacb7a7f083476
williamsfacundo <facuwilli@gmail.com>
2022-06-14 15:51:18 -0300

    Added canvas to the end game scene.

--- Witcher_War/Assets/Scripts/Witcher/Controller/Player_Status.cs
+++ Witcher_War/Assets/Scripts/Witcher/Controller/Player_Status.cs
@@ -12,6 +12,6 @@ public class Player_Status : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-                                                                
+                                                                        
     }
 }
