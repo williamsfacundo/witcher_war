25df872a7ead1da8a5c4d308e4aa59cf711ddfa8
williamsfacundo <facuwilli@gmail.com>
2022-06-28 14:53:38 -0300

    Added rotation static class.

---
+++ Witcher_War/Assets/Scripts/Witcher/Rotation/Rotation.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace WizardWar 
+{
+    namespace Witcher 
+    {
+        namespace Rotation 
+        {
+            public static class Rotation
+            {
+                
+            }
+        }
+    }
+}
