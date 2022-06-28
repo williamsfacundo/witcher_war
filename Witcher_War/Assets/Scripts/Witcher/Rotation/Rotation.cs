2d2801961ae7d6f14f28ecea656f16615922693f
williamsfacundo <facuwilli@gmail.com>
2022-06-28 15:18:34 -0300

    Added witcher rotation function.

--- Witcher_War/Assets/Scripts/Witcher/Rotation/Rotation.cs
+++ Witcher_War/Assets/Scripts/Witcher/Rotation/Rotation.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using WizardWar.Tile;
+using WizardWar.Enums;
 
 namespace WizardWar 
 {
@@ -8,7 +10,57 @@ namespace WizardWar
         {
             public static class Rotation
             {
-                
+                public static void RotateWitcher(GameObject witcher, Index2 rotationAxis, ref WitcherLookingDirection witcherDirection)
+                {
+                    if (rotationAxis != Index2.IndexZero) 
+                    {
+                        if (rotationAxis.X > 0)
+                        {
+                            witcherDirection = WitcherLookingDirection.Right;
+                        }
+                        else if (rotationAxis.X < 0)
+                        {
+                            witcherDirection = WitcherLookingDirection.Left;
+                        }
+                        else
+                        {
+                            if (rotationAxis.Y > 0)
+                            {
+                                witcherDirection = WitcherLookingDirection.Down;
+                            }
+                            else
+                            {
+                                witcherDirection = WitcherLookingDirection.Up;
+                            }
+                        }
+
+                        switch (witcherDirection)
+                        {
+                            case WitcherLookingDirection.Down:
+
+                                witcher.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                                break;
+                            case WitcherLookingDirection.Up:
+
+                                witcher.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+                                break;
+
+                            case WitcherLookingDirection.Right:
+
+                                witcher.transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+                                break;
+
+                            case WitcherLookingDirection.Left:
+
+                                witcher.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+                                break;
+                            default:
+
+                                witcher.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                                break;
+                        }
+                    }                    
+                }
             }
         }
     }
