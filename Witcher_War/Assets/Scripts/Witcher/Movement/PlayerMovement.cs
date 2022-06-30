503015052c57f17f9c9502c42fad3862512586b3
facundo <facuwilli@gmail.com>
2022-06-29 22:54:38 -0300

    Reincorporate freeze mechanic.

--- Witcher_War/Assets/Scripts/Witcher/Movement/PlayerMovement.cs
+++ Witcher_War/Assets/Scripts/Witcher/Movement/PlayerMovement.cs
@@ -13,41 +13,13 @@ namespace WizardWar
         {
             public class PlayerMovement : BasedMovement, IMovable
             {
-                //private const float _displacementTime = 0.3f;
+                private short _freezePos;               
 
-                private short _freezePos;
-
-                //private float _movementTimer;
-
-                //private float _percentageMoved;
-
-                private Index2 _movementAxis;
-
-                //private Index2 _nextTileIndex;                
-
-                //private Vector3 _oldPosition;
-
-                //private Vector3 _newPosition;                
-
-                //private Gameplay _gameplay;
+                private Index2 _movementAxis;                
 
                 public PlayerMovement() : base()
                 {
-                    _freezePos = 0;
-
-                    //_movementTimer = _displacementTime;
-
-                    //_percentageMoved = 0f;
-
-                    //_movementAxis = Index2.IndexZero;                    
-
-                    //_nextTileIndex = Index2.IndexZero;
-
-                    //_oldPosition = Vector3.zero;
-
-                    //_newPosition = Vector3.zero;
-
-                    //_gameplay = GameObject.FindWithTag("Manager").GetComponent<Gameplay>();                   
+                    _freezePos = 0;                                   
                 }
 
                 public void MoveInput()
@@ -59,17 +31,7 @@ namespace WizardWar
                         _movementAxis.Y = (short)Input.GetAxisRaw("Vertical");
 
                         _movementAxis.Y *= -1;
-                    }
-
-                    //if (_movementAxis.Y != 0)
-                    //{
-                    //    _movementAxis.Y *= -1;
-                    //}
-
-                    //if (_movementAxis.X != 0 && _movementAxis.Y != 0)
-                    //{
-                    //    _movementAxis.Y = 0;
-                    //}
+                    }                   
 
                     _freezePos = (short)Input.GetAxisRaw("Freeze");
                 }
@@ -81,62 +43,25 @@ namespace WizardWar
 
                 public void Move(GameObject witcher, ref WitcherLookingDirection direction)
                 {
-                    MoveFromCurrentTileToNewTile(witcher, _movementAxis, ref direction);
-                    //if (_movementAxis != Index2.IndexZero && !IsObjectMoving()) 
-                    //{
-                    //    if (_freezePos != 0) 
-                    //    {                           
-                    //        Rotation.RotateWitcher(witcher, _movementAxis, ref direction);
-                    //    }
-                    //    else 
-                    //    {
-                    //        _nextTileIndex = _gameplay.TileObjectsPositioningInTileMap.GetTileObjectIndexPlusOtherIndex(witcher, _movementAxis);
-                            
-                    //        if (_gameplay.TileObjectsInstanciator.LevelCreator.TileMap.IsTileEmpty(_nextTileIndex))
-                    //        {
-                    //            _movementTimer = 0f;
-
-                    //            _oldPosition = _gameplay.TileObjectsPositioningInTileMap.GetTileMapPosition(_gameplay.TileObjectsPositioningInTileMap.GetTileObjectIndex(witcher));
-
-                    //            _oldPosition.y = witcher.transform.position.y;
-
-                    //            _newPosition = _gameplay.TileObjectsPositioningInTileMap.GetTileMapPosition(_nextTileIndex);
-
-                    //            _newPosition.y = witcher.transform.position.y;
-
-                    //            _gameplay.TileObjectsPositioningInTileMap.MoveGameObjectToTileX(_nextTileIndex, witcher);                              
-                    //        }
-
-                    //        Rotation.RotateWitcher(witcher, _movementAxis, ref direction);                            
-                    //    }
-                    //}        
-
-                    //if (IsObjectMoving())
-                    //{
-                    //    _percentageMoved = _movementTimer / _displacementTime;
-
-                    //    witcher.transform.position = Vector3.Lerp(_oldPosition, _newPosition, _percentageMoved);                        
-                    //}
+                    if (_freezePos == 0) 
+                    {
+                        MoveFromCurrentTileToNewTile(witcher, _movementAxis, ref direction);
+                    }
+                    else if (_movementAxis != Index2.IndexZero)                  
+                    {
+                        Rotation.RotateWitcher(witcher, _movementAxis, ref direction);
+                    }
+                                        
                 }   
 
                 public void Timer() 
                 {
-                    UpdateMovementTimer();
-                    //if (_movementTimer < _displacementTime) 
-                    //{
-                    //    _movementTimer += Time.deltaTime;
-
-                    //    if (_movementTimer > _displacementTime)
-                    //    {
-                    //        _movementTimer = _displacementTime;
-                    //    }            
-                    //}        
+                    UpdateMovementTimer();                       
                 }   
 
                 public bool IsObjectMoving()
                 {
-                    return IsMoving();
-                    //return _movementTimer < _displacementTime;
+                    return IsMoving();                   
                 }                           
             }
         }
